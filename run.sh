0ebd00727ea2b30304f031068c5c925d9d577b54
Igor Cavalcanti <cavalcantigor@gmail.com>
2019-07-14 14:35:58 -0300

    Refatorando nome da aplicacao.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "=> Instalando pacotes de requirements.txt necessarios a aplicacao flaskeleton..."
+echo "=> Instalando pacotes de requirements.txt necessarios a aplicacao flaskeleton-api..."
 pip install -r requirements.txt
 echo ""
 echo "=> O waitress-server deu inicio ao processo de deploy da aplicação..."
