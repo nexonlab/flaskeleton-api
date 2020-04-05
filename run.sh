7b1cbcb2bc8aed6fd249077d717f5b26b4dec2ef
Igor Cavalcanti <igor.lima@luizalabs.com>
2020-04-05 00:37:33 -0300

    Corrigindo comando para iniciar gunicorn

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 echo "=> Instalando pacotes de requirements.txt necessarios a aplicacao flaskeleton-api..."
 pip install -r requirements.txt
 echo ""
-echo "=> O waitress-server deu inicio ao processo de deploy da aplicação..."
-gunicorn --workers=4 --bind=0.0.0.0:5000 --log-level=info  app:'create_app()'
+echo "=> Iniciando gunicorn..."
+gunicorn  --worker-class gevent --workers=4 --threads 50 --bind=0.0.0.0:5000 --log-level=info app:'create_app()'
